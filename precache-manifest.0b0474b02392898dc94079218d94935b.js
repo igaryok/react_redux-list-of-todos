531072c116aadbe232d8c85347c9f9819b52651d
igaryok <igor.sobko@gmail.com>
2019-10-05 11:30:38 +0300

    Updates

---
+++ precache-manifest.0b0474b02392898dc94079218d94935b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52577980ad7607f2b93cd0251b49075",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "2cc04e7bb254291a5b77",
+    "url": "/react_redux-list-of-todos/static/css/main.ace76844.chunk.css"
+  },
+  {
+    "revision": "30fe23808ce834e6d1d5",
+    "url": "/react_redux-list-of-todos/static/js/2.be29157e.chunk.js"
+  },
+  {
+    "revision": "2cc04e7bb254291a5b77",
+    "url": "/react_redux-list-of-todos/static/js/main.ed2c7bbe.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
